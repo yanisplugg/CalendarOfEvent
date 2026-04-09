db56e4abe4e06a5a89a6100dce8c46cc032d61cb
desyatskii <desyatskii@gmail.com>
2026-04-09 07:52:18 +0300

    Добавлены классы Event и CalendarManager

---
+++ Calendar/CalendarManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+public class CalendarManager
+{
+    public List<Event> Events { get; private set; }
+    public CalendarManager()
+    {
+        Events = new List<Event>();
+        LoadEvents();
+    }
+    public void AddEvent(Event e)
+    {
+        if (e == null)
+        {
+            throw new ArgumentNullException(nameof(e));
+        }
+        Events.Add(e);
+        SaveEvents();
+    }
+    public void RemoveEvent(Event e)
+    {
+        if (e == null)
+        {
+            throw new ArgumentNullException(nameof(e));
+        }
+        Events.Remove(e);
+        SaveEvents();
+    }
+    public void SaveEvents()
+    {
+        File.WriteAllLines("events.txt", Events.Select(e =>
+        $"{e.Date.ToString("yyyy-MM-dd")}|{e.Description}"));
+    }
+    public void LoadEvents()
+    {
+        if (File.Exists("events.txt"))
+        {
+            var lines = File.ReadAllLines("events.txt");
+            foreach (var line in lines)
+            {
+                var parts = line.Split('|');
+                if (parts.Length == 2)
+                {
+                    DateTime date;
+                    if (DateTime.TryParse(parts[0], out date))
+                    {
+                        Events.Add(new Event(date, parts[1]));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Calendar/Event.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class Event
+{
+    public DateTime Date { get; set; }
+    public string Description { get; set; }
+    public Event(DateTime date, string description)
+    {
+        Date = date;
+        Description = description;
+    }
+}
